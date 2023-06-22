59a2040457cb3ee12355bd416e8fa47de0ecc519
linanyang <nanyeoe@gmail.com>
2023-06-22 21:40:56 +0800

    Update _config.anzhiyu.yml from Github Actions at 2023-06-22 21:40:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["/happy2023#pt_1674419635079.html","f729aefc0b3f.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","7118bedfce3e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["/happy2023#pt_1674419635079.html","f729aefc0b3f.html","8d4c30ce2923.html","ded755f9f77a.html","7118bedfce3e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
