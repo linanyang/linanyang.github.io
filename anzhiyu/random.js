48a28ef06df8efa5c7e0c305aa2e01dd5c2db66e
linanyang <nanyeoe@gmail.com>
2023-06-22 20:54:53 +0800

    Update hello-world.md from Github Actions at 2023-06-22 20:54:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["/happy2023#pt_1674419635079.html","f729aefc0b3f.html","09f024682a1a.html","7eeddef4f7b3.html","8d4c30ce2923.html","ded755f9f77a.html","7118bedfce3e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["/happy2023#pt_1674419635079.html","f729aefc0b3f.html","09f024682a1a.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","7118bedfce3e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
