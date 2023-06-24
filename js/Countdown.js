d08f1072214e18771a1cbedc93ba991924504518
linanyang <nanyeoe@gmail.com>
2023-06-24 19:01:48 +0800

    Update Countdown.js from Github Actions at 2023-06-24 19:00:57

--- js/Countdown.js
+++ js/Countdown.js
@@ -3,7 +3,7 @@ var Countdown = () => {
     clearTimeout(CountdownTimer);
     if (!document.querySelector('#Countdown')) return;
     // 截至时间戳 and 星期对象
-    let Countdown = new Date('2024-06-07 00:00:00').getTime() / 1000,
+    let Countdown = new Date('2024-06-07 00:00:00').getTime(+8) / 1000,
         week = { 0: '周日', 1: '周一', 2: '周二', 3: '周三', 4: '周四', 5: '周五', 6: '周六' }
 
     time();
@@ -16,10 +16,10 @@ var Countdown = () => {
         let now = new Date();
 
         // 右下角 今天
-        document.querySelector('#Countdown .today').innerHTML = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + week[now.getDay()]
+        document.querySelector('#Countdown .today').innerHTML = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate(+8) + ' ' + week[now.getDay()]
 
         // 现在与截至时间相差秒数
-        let second = Countdown - Math.round(now.getTime() / 1000);
+        let second = Countdown - Math.round(now.getTime(+8) / 1000);
 
         // 小于0则表示已经到截至时间
         if (second < 0) {
