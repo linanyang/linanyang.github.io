8b29860bce3dcba9bad30e11c8ac961b52bf079e
linanyang <nanyeoe@gmail.com>
2023-06-24 22:11:18 +0800

    Update Countdown.js from Github Actions at 2023-06-24 22:10:19

--- js/Countdown.js
+++ js/Countdown.js
@@ -3,7 +3,7 @@ var Countdown = () => {
     clearTimeout(CountdownTimer);
     if (!document.querySelector('#Countdown')) return;
     // 截至时间戳 and 星期对象
-    let Countdown = new Date('2023-06-07 09:00:00').getTime() / 1000,
+    let Countdown = new Date('2024-06-07 09:00:00').getTime() / 1000,
         week = { 0: '周日', 1: '周一', 2: '周二', 3: '周三', 4: '周四', 5: '周五', 6: '周六' }
 
     time();
