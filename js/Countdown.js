ad60131b360dd0f546b6dbe6390b40a2eb495f0c
linanyang <nanyeoe@gmail.com>
2023-06-24 13:40:48 +0800

    062402 from Github Actions at 2023-06-24 13:39:59

--- js/Countdown.js
+++ js/Countdown.js
@@ -2,8 +2,8 @@ let CountdownTimer = null;
 var Countdown = () => {
     clearTimeout(CountdownTimer);
     if (!document.querySelector('#Countdown')) return;
-    // 新年时间戳 and 星期对象
-    let Countdown = new Date('2023-01-22 00:00:00').getTime() / 1000,
+    // 截至时间戳 and 星期对象
+    let Countdown = new Date('2024-06-07 00:00:00').getTime() / 1000,
         week = { 0: '周日', 1: '周一', 2: '周二', 3: '周三', 4: '周四', 5: '周五', 6: '周六' }
 
     time();
@@ -18,16 +18,16 @@ var Countdown = () => {
         // 右下角 今天
         document.querySelector('#Countdown .today').innerHTML = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + week[now.getDay()]
 
-        // 现在与新年相差秒数
+        // 现在与截至时间相差秒数
         let second = Countdown - Math.round(now.getTime() / 1000);
 
-        // 小于0则表示已经过年
+        // 小于0则表示已经到截至时间
         if (second < 0) {
-            document.querySelector('#Countdown .title').innerHTML = 'Happy New Year!';
-            document.querySelector('#Countdown .Countdown-time').innerHTML = '<span class="happyCountdown">新年快乐</p>';
+            document.querySelector('#Countdown .title').innerHTML = '高考加油！';
+            document.querySelector('#Countdown .Countdown-time').innerHTML = '<span class="happyCountdown">金榜题名</p>';
         } else {
-            // 大于0则还未过年
-            document.querySelector('#Countdown .title').innerHTML = '距离2023年春节：'
+            // 大于0则还未到截止时间
+            document.querySelector('#Countdown .title').innerHTML = '距离2024年高考：'
 
             // 大于一天倒计时
             if (second > 86400) {
@@ -44,17 +44,5 @@ var Countdown = () => {
             }
         }
     }
-
-    // 元宝飘落
-    jQuery(document).ready(function($) {
-        $('#Countdown').wpSuperSnow({
-            flakes: ['https://z4a.net/images/2023/01/02/yb1.webp', 'https://z4a.net/images/2023/01/02/yb2.webp', 'https://z4a.net/images/2023/01/02/yb3.webp', 'https://z4a.net/images/2023/01/02/59dfc0c83d7faa533c46abf6f9076a45-removebg-preview.png'],
-            totalFlakes: '100',
-            zIndex: '999999',
-            maxSize: '30',
-            maxDuration: '20',
-            useFlakeTrans: false
-        });
-    });
 }
 Countdown();
