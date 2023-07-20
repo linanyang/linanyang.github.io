91224c29ce28928bcb03cc436f6b8f9704849608
linanyang <nanyeoe@gmail.com>
2023-07-20 22:57:05 +0800

    Update about.yml from Github Actions at 2023-07-20 22:56:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["287278eb0e01.html","f729aefc0b3f.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","7118bedfce3e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["287278eb0e01.html","f729aefc0b3f.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","cd2e4c752297.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
