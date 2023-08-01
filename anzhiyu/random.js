ba7eca06ec5fc637f7d2bd854323684baeeeabe6
linanyang <nanyeoe@gmail.com>
2023-08-01 23:43:33 +0800

    nav from Github Actions at 2023-08-01 23:42:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["287278eb0e01.html","f729aefc0b3f.html","0ee43e949db5.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","cd2e4c752297.html","7566aaafe2de.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["287278eb0e01.html","f729aefc0b3f.html","0ee43e949db5.html","4cd9f71c96dc.html","8d4c30ce2923.html","ded755f9f77a.html","cd2e4c752297.html","930405e221e1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
