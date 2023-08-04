5735e6d1fef74a9d67c3326c568cb4356d74421f
linanyang <nanyeoe@gmail.com>
2023-08-04 10:00:56 +0800

    Update package.json from Github Actions at 2023-08-04 10:00:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["287278eb0e01.html","f729aefc0b3f.html","0ee43e949db5.html","4cd9f71c96dc.html","ecd2a6eb5d4c.html","8d4c30ce2923.html","ded755f9f77a.html","cd2e4c752297.html","930405e221e1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["287278eb0e01.html","f729aefc0b3f.html","0ee43e949db5.html","4cd9f71c96dc.html","4b9dcb4574a4.html","8d4c30ce2923.html","ded755f9f77a.html","cd2e4c752297.html","930405e221e1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
